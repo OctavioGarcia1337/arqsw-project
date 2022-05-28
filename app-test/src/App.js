230c538c01007012ee396ab35b7714f16d3f5cb2
Octavio Garcia <80827553+OctavioGarcia1337@users.noreply.github.com>
2022-05-28 17:17:38 -0300

    start commit

---
+++ app-test/src/App.js
@@ -0,0 +1,15 @@
+import ShoppingCart from "./components/ShoppingCart";
+
+function App() {
+  return (
+    <div>
+      <hr />
+      <ShoppingCart />
+      <hr />
+    </div>
+  );
+}
+
+export default App;
+
+
