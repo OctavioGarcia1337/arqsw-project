1de271e5cc115e08cc0bc34d90dfdfd162574291
Octavio Garcia <80827553+OctavioGarcia1337@users.noreply.github.com>
2022-06-04 06:40:50 -0300

    Add files via upload

---
+++ login/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/OctavioGarcia1337/login/controllers"
+	"github.com/gofiber/fiber/v2"
+)
+
+func Setup(app *fiber.App) {
+
+	app.Post("/api/register", controllers.Register)
+	app.Post("/api/login", controllers.Login)
+	app.Get("/api/user", controllers.User)
+	app.Post("/api/logout", controllers.Logout)
+}
