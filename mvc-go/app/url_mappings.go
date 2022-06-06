ec1efc886ca00345df559d874c089f78b79a6a8a
Matías Lessio <2003159@ucc.edu.ar>
2022-06-05 22:27:12 -0300

    url mappings

--- mvc-go/app/url_mappings.go
+++ mvc-go/app/url_mappings.go
@@ -1,6 +1,10 @@
 package app
 
 import (
+	addressController "mvc-go/controllers/address"
+	categoryController "mvc-go/controllers/category"
+	orderController "mvc-go/controllers/order"
+	orderDetailController "mvc-go/controllers/order_detail"
 	productController "mvc-go/controllers/product"
 	userController "mvc-go/controllers/user"
 
@@ -8,14 +12,33 @@ import (
 )
 
 func mapUrls() {
-	// Products Mapping
-	router.GET("/product/:product_id", productController.GetProductByEan)
-	router.GET("/product", productController.GetProducts)
-
 	// Users Mapping
 	router.GET("/user/:id", userController.GetUserById)
 	router.GET("/user", userController.GetUsers)
-	router.POST("/user", userController.UserInsert)
+	router.POST("/users/login", userController.LoginUser) //hay que ver como lo hacemos bien
+
+	//Address Mapping
+	router.GET("/address/:id", addressController.GetAddressById)
+	router.GET("/address", addressController.GetAddresses)
+
+	//Category Mapping
+	router.GET("/category/:id", categoryController.GetCategoryById)
+	router.GET("/category", categoryController.GetCategories)
+
+	//Order Mapping
+	router.GET("/order/:id", orderController.GetOrderById)
+	router.GET("/order", orderController.GetOrders)
+	router.POST("/order", orderController.OrderInsert)
+	router.GET("/orderUser/:idUser", orderController.GetOrdersByIdUser)
+
+	//OrderDetail Mapping
+	router.GET("/orderDetail/:id", orderDetailController.GetOrderDetailById)
+	router.GET("/orderDetail", orderDetailController.GetOrderDetails)
+	router.POST("/orderDetail", orderDetailController.OrderDetailInsert)
+
+	//Product Mapping
+	router.GET("/product/:id", productController.GetProductById)
+	router.GET("/product", productController.GetProducts)
 
 	log.Info("Finishing mappings configurations")
 }
