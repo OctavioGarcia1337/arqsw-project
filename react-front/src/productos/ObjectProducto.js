4a0a2dbf18c62f16558ea435d348dfbc1b07d09f
Matías Lessio <2003159@ucc.edu.ar>
2022-06-08 12:05:42 -0300

    Lista de productos

---
+++ react-front/src/productos/ObjectProducto.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export const ObjectProducto =(
+    {id,
+    name,
+    base_price,
+    stock,
+    description,
+    id_category
+})=>{
+    return(
+        <div>
+        <a href="#">
+        </a>
+        <div>
+            <h1>{name}</h1>
+            <p>{description}</p>
+            <p>Stock: {stock}</p>
+            <p>${base_price}</p>
+        </div>
+        <div>
+            <button>
+                Agregar al carrito
+            </button>
+        </div>
+        </div>
+    )
+}
---
+++ react-front/src/productos/Productos.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState} from "react";
+import { ObjectProducto } from "./ObjectProducto";
+
+
+export const Productos = ()=>{
+
+    const [productos,setProductos] = useState([]);
+    const [prodseach,setProdsearch] = useState([]);
+    const [busqueda, setBusqueda]= useState("");
+    const fetchApi = async()=>{
+    const response = await fetch('http://localhost:8090/product')
+    .then((response) => response.json());
+    setProductos(response);
+    setProdsearch(response);
+    };
+    useEffect(()=>{
+    fetchApi();
+    },[])
+    const handleChange=e=>{
+        setBusqueda(e.target.value);
+        filtrar(e.target.value);
+      }
+      const filtrar=(terminoBusqueda)=>{
+        var resultadosBusqueda=prodseach.filter((elemento)=>{
+          if(elemento.name.toString().toLowerCase().includes(terminoBusqueda.toLowerCase()))
+          {
+            return elemento;
+          }
+        });
+        setProductos(resultadosBusqueda);
+      }
+
+    return(
+        <>
+        <h1> LISTA DE PRODUCTOS</h1>
+        <div>
+        <input
+          value={busqueda}
+          placeholder="Interesado en algo?"
+          onChange={handleChange}
+        />
+      </div>
+
+        <div>
+            {
+                productos.map(producto =>(
+                  <ObjectProducto key={producto.id}
+                  id={producto.id}
+                  name={producto.name}
+                  base_price={producto.base_price}
+                  stock={producto.stock}
+                  description={producto.description}
+                  />  
+                ))
+            }
+        </div>
+        </>
+    )
+}
